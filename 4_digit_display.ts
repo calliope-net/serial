de8c63dc2775f96780bcd41668ee780dde7a7cf2
calliope-net <141074597+calliope-net@users.noreply.github.com>
2025-11-08 19:39:00 +0100

    Aktualisiere 4_digit_display.ts

--- 4_digit_display.ts
+++ 4_digit_display.ts
@@ -32,57 +32,19 @@ namespace serial { /* 4_digit_display.ts
     }
 
 
-
+    // ==========
     export class TM1637 {
-        clkPin: DigitalPin;
-        dataPin: DigitalPin;
-        brightnessLevel: number;
-        pointFlag: boolean;
-        buf: Buffer;
+        clkPin: DigitalPin
+        dataPin: DigitalPin
+        brightnessLevel: number
+        pointFlag: boolean
+        buf: Buffer
 
-        private writeByte(wrData: number) {
-            for (let i = 0; i < 8; i++) {
-                pins.digitalWritePin(this.clkPin, 0);
-                if (wrData & 0x01) pins.digitalWritePin(this.dataPin, 1);
-                else pins.digitalWritePin(this.dataPin, 0);
-                wrData >>= 1;
-                pins.digitalWritePin(this.clkPin, 1);
-            }
 
-            pins.digitalWritePin(this.clkPin, 0); // Wait for ACK
-            pins.digitalWritePin(this.dataPin, 1);
-            pins.digitalWritePin(this.clkPin, 1);
-        }
+        // ========== group="4-Ziffern Display" subcategory="4-Digit Display"
 
-        private start() {
-            pins.digitalWritePin(this.clkPin, 1);
-            pins.digitalWritePin(this.dataPin, 1);
-            pins.digitalWritePin(this.dataPin, 0);
-            pins.digitalWritePin(this.clkPin, 0);
-        }
-
-        private stop() {
-            pins.digitalWritePin(this.clkPin, 0);
-            pins.digitalWritePin(this.dataPin, 0);
-            pins.digitalWritePin(this.clkPin, 1);
-            pins.digitalWritePin(this.dataPin, 1);
-        }
-
-        private coding(dispData: number): number {
-            let pointData = 0;
-
-            if (this.pointFlag == true) pointData = 0x80;
-            else if (this.pointFlag == false) pointData = 0;
-
-            if (dispData == 0x7f) dispData = 0x00 + pointData;
-            else dispData = TubeTab[dispData] + pointData;
-
-            return dispData;
-        }
-
-        //% group="4-Digit" subcategory="4-Digit Display"
-        //% block="%Display Zahl 0..9999 anzeigen %dispData"
-        // blockId=grove_tm1637_display_number 
+        //% group="4-Ziffern Display" subcategory="4-Digit Display"
+        //% block="%Display Zahl 0..9999 anzeigen %dispData" weight=9
         show(dispData: number) {
             let compare_01: number = dispData % 100;
             let compare_001: number = dispData % 1000;
@@ -144,21 +106,9 @@ namespace serial { /* 4_digit_display.ts
             }
         }
 
-        //% group="4-Digit" subcategory="4-Digit Display"
-        //% block="%Display Helligkeit %level 0..7"
-        // blockId=grove_tm1637_set_display_level 
-        //% level.min=0 level.max=7
-        set(level: number) {
-            this.brightnessLevel = level;
-            this.bit(this.buf[0], 0x00);
-            this.bit(this.buf[1], 0x01);
-            this.bit(this.buf[2], 0x02);
-            this.bit(this.buf[3], 0x03);
-        }
 
-        //% group="4-Digit" subcategory="4-Digit Display"
-        //% block="%Display Ziffer 0..9 anzeigen %dispData an Stelle %bitAddr "
-        // blockId=grove_tm1637_display_bit 
+        //% group="4-Ziffern Display" subcategory="4-Digit Display"
+        //% block="%Display Ziffer 0..9 anzeigen %dispData an Stelle %bitAddr" weight=7
         //% dispData.min=0 dispData.max=9
         //% bitAddr.min=0 bitAddr.max=3
         bit(dispData: number, bitAddr: eZiffer) {
@@ -181,8 +131,9 @@ namespace serial { /* 4_digit_display.ts
             }
         }
 
-        //% group="4-Digit" subcategory="4-Digit Display"
-        //% block="%Display Doppelpunkt %point"
+
+        //% group="4-Ziffern Display" subcategory="4-Digit Display"
+        //% block="%Display Doppelpunkt %point" weight=6
         // blockId=grove_tm1637_display_point 
         //%  point.shadow=toggleOnOff
         point(point: boolean) {
@@ -194,15 +145,72 @@ namespace serial { /* 4_digit_display.ts
             this.bit(this.buf[3], 0x03);
         }
 
-        //% group="4-Digit" subcategory="4-Digit Display"
-        //% block="%Display löschen"
-        // blockId=grove_tm1637_display_clear 
+
+        //% group="4-Ziffern Display" subcategory="4-Digit Display"
+        //% block="%Display löschen" weight=4
         clear() {
             this.bit(0x7f, 0x00);
             this.bit(0x7f, 0x01);
             this.bit(0x7f, 0x02);
             this.bit(0x7f, 0x03);
         }
+
+
+        //% group="4-Ziffern Display" subcategory="4-Digit Display"
+        //% block="%Display Helligkeit %level 0..7" weight=2
+        //% level.min=0 level.max=7
+        set(level: number) {
+            this.brightnessLevel = level;
+            this.bit(this.buf[0], 0x00);
+            this.bit(this.buf[1], 0x01);
+            this.bit(this.buf[2], 0x02);
+            this.bit(this.buf[3], 0x03);
+        }
+
+
+
+        // ========== private
+
+        private writeByte(wrData: number) {
+            for (let i = 0; i < 8; i++) {
+                pins.digitalWritePin(this.clkPin, 0);
+                if (wrData & 0x01) pins.digitalWritePin(this.dataPin, 1);
+                else pins.digitalWritePin(this.dataPin, 0);
+                wrData >>= 1;
+                pins.digitalWritePin(this.clkPin, 1);
+            }
+
+            pins.digitalWritePin(this.clkPin, 0); // Wait for ACK
+            pins.digitalWritePin(this.dataPin, 1);
+            pins.digitalWritePin(this.clkPin, 1);
+        }
+
+        private start() {
+            pins.digitalWritePin(this.clkPin, 1);
+            pins.digitalWritePin(this.dataPin, 1);
+            pins.digitalWritePin(this.dataPin, 0);
+            pins.digitalWritePin(this.clkPin, 0);
+        }
+
+        private stop() {
+            pins.digitalWritePin(this.clkPin, 0);
+            pins.digitalWritePin(this.dataPin, 0);
+            pins.digitalWritePin(this.clkPin, 1);
+            pins.digitalWritePin(this.dataPin, 1);
+        }
+
+        private coding(dispData: number): number {
+            let pointData = 0;
+
+            if (this.pointFlag == true) pointData = 0x80;
+            else if (this.pointFlag == false) pointData = 0;
+
+            if (dispData == 0x7f) dispData = 0x00 + pointData;
+            else dispData = TubeTab[dispData] + pointData;
+
+            return dispData;
+        }
+
     }
 
     let TubeTab: number[] = [
